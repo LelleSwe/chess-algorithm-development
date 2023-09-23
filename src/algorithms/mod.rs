120360fdcdca76162807fc41daa1d726afef884a
confusinguser <25439218+confusinguser@users.noreply.github.com>
2023-09-23 18:31:46 +0200

    Rename algorithms to add version number

--- src/algorithms/mod.rs
+++ src/algorithms/mod.rs
@@ -1,3 +1,4 @@
-pub(crate) mod basic;
-pub(crate) mod basic_no_stalemate;
-pub(crate) mod random_move;
+pub(crate) mod v0_random_move;
+pub(crate) mod v1_basic;
+pub(crate) mod v2_basic_no_stalemate;
+pub(crate) mod v3_alpha_beta_pruning;
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
-use chess::{Action, Board, Game};
+use crate::algorithms::v0_random_move::RandomMove;
+use crate::algorithms::v1_basic::BasicAlgo;
+use crate::algorithms::v2_basic_no_stalemate::BasicNoStalemateAlgo;
 
-use crate::algorithms::basic_no_stalemate::BasicNoStalemateAlgo;
-use crate::common::utils;
-use crate::pitter::logic::{GameOutcome, GamePairOutcome};
-
-use self::algorithms::random_move::RandomMove;
 use self::pitter::logic::Competition;
 
 mod algorithms;
@@ -12,7 +9,7 @@ mod common;
 mod pitter;
 
 fn main() {
-    type Algo1 = RandomMove;
+    type Algo1 = BasicAlgo;
     type Algo2 = BasicNoStalemateAlgo;
 
     let mut competition = Competition::new(Box::new(Algo1 {}), Box::new(Algo2 {}));
